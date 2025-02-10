308260619cc5dfc329088609de2e152165525c69
EldenGlitchDev <benjamin.delplanque@hotmail.fr>
2025-02-10 16:00:45 +0100

    VillageGreen

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class TestController extends AbstractController
+final class ContactController extends AbstractController
 {
-    #[Route('/', name: 'app_test')]
+    #[Route('/contact', name: 'app_contact')]
     public function index(): Response
     {
-        return $this->render('test/index.html.twig', [
-            'controller_name' => 'EldenGlitchDev',
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
         ]);
     }
 }
