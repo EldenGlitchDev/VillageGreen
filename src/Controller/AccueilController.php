e1a7ce77acdaf39b157fdda598e4851ef50d709d
EldenGlitchDev <benjamin.delplanque@hotmail.fr>
2025-03-03 15:09:20 +0100

    vue accueil

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -5,9 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\CategorieRepository;
 
 final class AccueilController extends AbstractController
 {
+
+    /*private $categorieRepository;
+
+    public function __construct(CategorieRepository $categorieRepository)
+    {
+        $this->categorieRepository=$categorieRepository;
+    }*/
+
+
     #[Route('/', name: 'app_accueil')]
     public function index(): Response
     {
@@ -15,4 +25,25 @@ final class AccueilController extends AbstractController
             'controller_name' => 'AccueilController',
         ]);
     }
+    
+    /*#[Route('/categorie', name: 'app_categorie')]
+    public function categorie(CategorieRepository $categorieRepository): Response
+    {
+        $categorie = $categorieRepository->findAll();
+        return $this->render('accueil/index.html.twig', [
+            'categorie' => $categorie,
+        ]);
+    }*/
+
+    /*#[Route('/categorie', name: 'app_categorie')]
+    public function categorie()
+{
+    $categories = $this->getDoctrine()->getRepository(CategorieRepository::class)->findAll();
+
+    return $this->render('votre_template.html.twig', [
+        'categories' => $categories
+    ]);
+}*/
+
+    
 }
