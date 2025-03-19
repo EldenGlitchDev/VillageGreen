ae51ec38e480a5c2c3ac02bffb6510967ac815ee
EldenGlitchDev <benjamin.delplanque@hotmail.fr>
2025-03-19 15:21:04 +0100

    finir contact

---
+++ src/Controller/MentionsLegPolConfController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class MentionsLegPolConfController extends AbstractController
+{
+    #[Route('/mentions/leg', name: 'app_mentions_leg')]
+    public function MentionsLeg(): Response
+    {
+        return $this->render('mentions_leg_pol_conf/index.html.twig', [
+            'controller_name' => 'MentionsLegPolConfController',
+        ]);
+    }
+
+    #[Route('/pol/conf', name: 'app_politique_conf')]
+    public function PolConf(): Response
+    {
+        return $this->render('mentions_leg_pol_conf/polConf.html.twig', [
+            'controller_name' => 'MentionsLegPolConfController',
+        ]);
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function Contact(): Response
+    {
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'MentionsLegPolConfController',
+        ]);
+    }
+}
